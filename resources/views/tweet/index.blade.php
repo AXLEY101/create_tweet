c930db3b48296c21f2269b039e2ef861b28eb7c7
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-08-25 14:39:06 +0000

    コンポーネントを利用してlayoutを作成

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -1,49 +1,45 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <title>つぶやいたー</title>
-    </head>
-    <body>
-        @if($errors->any())
-        <div>
-            @foreach($errors->all() as $error)
-                {{ $error }}<br>
-            @endforeach
-        </div>
-        @endif
+
         
-        
-        <h1>つぶやいたー</h1>
-        @if(session('feedback.success'))
-            <p style="color: green">{{ session('feedback.success') }}</p>
-        @endif
-        @auth
+        <!-- 今回はLaravel８からのコンポーネントを利用したlayoutを利用。　アドレスは/resources/views/components/layout.blade.php になるので注意。　感覚としては継承と似てるがlayoutを置いておく場所が全然場所違うので注意 -->
+        <x-layout title="TOP | つぶやいたー">
+                @if($errors->any())
             <div>
-                <p>投稿フォーム</p>
-                <form action="/tweet/create" method="post">
-                    @csrf
-                    <label for="tweet-content">つぶやき</label>
-                        <span>１４０文字まで</span><br>
-                        <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea><br>
-                    <button type="submit">投稿</button>
-                </form>
+                @foreach($errors->all() as $error)
+                    {{ $error }}<br>
+                @endforeach
             </div>
-        @endauth
-        <div>
-        @foreach($tweets as $tweet)
-            <details>
-                <summary>{{ $tweet->content }} by {{ $tweet->user->name }}</summary>
-                @if(\Illuminate\Support\Facades\Auth::id() === $tweet->user_id)
+            @endif
+            
+            
+            
+            <h1>つぶやいたー</h1>
+            @if(session('feedback.success'))
+                <p style="color: green">{{ session('feedback.success') }}</p>
+            @endif
+            @auth
                 <div>
-                    <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                    <p>投稿フォーム</p>
+                    <form action="/tweet/create" method="post">
+                        @csrf
+                        <label for="tweet-content">つぶやき</label>
+                            <span>１４０文字まで</span><br>
+                            <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea><br>
+                        <button type="submit">投稿</button>
+                    </form>
                 </div>
-                @else
-                    編集できません。
-                @endif
-            </details>
-        @endforeach   
-        </div>
-    </body>
-</html>
+            @endauth
+            <div>
+            @foreach($tweets as $tweet)
+                <details>
+                    <summary>{{ $tweet->content }} by {{ $tweet->user->name }}</summary>
+                    @if(\Illuminate\Support\Facades\Auth::id() === $tweet->user_id)
+                    <div>
+                        <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                    </div>
+                    @else
+                        編集できません。
+                    @endif
+                </details>
+            @endforeach   
+            </div>
+        </x-layout>
