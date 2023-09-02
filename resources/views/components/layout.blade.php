fdac9cb914f6eebf3f8c07fd8f4d7529ba2b983a
EC2 Default User <ec2-user@ip-172-31-28-54.ap-northeast-1.compute.internal>
2023-09-02 06:20:13 +0000

    CSSをviteで

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <!-- 今回vite使用のため、下記のlink href=""とscript src="" はデフォルトでキャッシュバスティングされている。 mixとの違いに注意 -->
-        <link href="/css/app.css" rel="stylesheet">
-        <script src="/js/app.js" async defer></script>
+        <!-- 今回vite使用のため、デフォルトでキャッシュバスティングされている。 mixとの違いに注意 -->
+        
+        @vite('resources/css/app.css')
+        @vite('resources/js/app.js')
         <!-- async　defer　両方記述したのは、古いブラウザでは片方しか対応していない時、対応できるため。　両方認識できる場合はasyncが優先されることにも注意。 -->
         <!-- asyncの挙動 スクリプトは非同期的にダウンロードされます。これは、HTMLのパースが一時停止されることなく、スクリプトがバックグラウンドでダウンロードされることを意味する -->
         <!-- deferの挙動 スクリプトも非同期的にダウンロードされますが、HTMLのパースが完了するまでスクリプトの実行は待機されます -->
